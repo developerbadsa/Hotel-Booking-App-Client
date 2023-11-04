76b8fcb477b1fee3d63b4ed9cee841eac9a15e67
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-11-04 22:07:00 +0600

    react helmet installed and using for dynamic change title fav icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Helmet } from 'react-helmet'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>hello</h1>
+    <h1>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="icon" type="image/png" href="/public/home.png" />
+      </Helmet>
+
+    hello
+    <img src="/public/home.png" alt="" />
+    </h1>
   </React.StrictMode>,
 )
